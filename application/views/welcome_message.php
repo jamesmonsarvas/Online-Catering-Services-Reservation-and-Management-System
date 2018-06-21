61679a2a1fe84fbfc8dd720c6bdaa9744300955d
DESKTOP-FHGK2GA\JamesLene09 <jamez.monsarvas@gmail.com>
2018-06-21 16:01:06 +0800

    no message

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -76,9 +76,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 	</div>
 
