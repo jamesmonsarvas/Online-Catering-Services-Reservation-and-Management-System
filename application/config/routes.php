107786ffd019d40f8082571c644ef155e7641960
James Monsarvas <jamez.monsarvas@gmail.com>
2018-09-04 12:36:51 +0800

    no message

--- application/config/routes.php
+++ application/config/routes.php
@@ -8,20 +8,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $route['auth/register'] = 'auth/create_user';
 // $route['auth/forgot-password'] = 'auth/create_user';
 
-<<<<<<< HEAD
 $route['admin/packages/update'] = 'admin/packages/update';
 $route['admin/packages/create'] = 'admin/packages/create';
 $route['admin/packages'] = 'admin/packages/index';
-=======
+
 $route['admin'] = 'admin/dashboard';
->>>>>>> 22452f9f7fb18cd561795e30b314916bd3b9c814
 
 $route['admin/services/update'] = 'admin/services/update';
 $route['admin/services/create'] = 'admin/services/create';
 $route['admin/services'] = 'admin/services/index';
 
-// $route['reservation/update'] = 'reservation_output/update';
-// $route['reservation/create'] = 'reservation_output/create';
+$route['reservation/update'] = 'reservation_output/update';
+$route['reservation/create'] = 'reservation_output/create';
 
 $route['reservation'] = 'reservation_output/view';
 $route['services'] = 'services_output/view';
