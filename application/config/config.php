89bc993df26988b4ffe7995087d93117f395b7f4
MharikAngelo <ma.gutlay1012@gmail.com>
2018-11-15 10:38:10 +0800

    change config base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Online-Catering-Services-Reservation-and-Management-System/';
+$config['base_url'] = 'http://kirstian.local/';
 
 /*
 |--------------------------------------------------------------------------
