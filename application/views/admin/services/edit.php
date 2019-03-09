6e04e27a6531c10a8005b64e5deb5fb5e5db6897
RDelaCruz00 <renzdelacruz00@gmail.com>
2019-03-09 22:21:51 +0800

    - Fix div

--- application/views/admin/services/edit.php
+++ application/views/admin/services/edit.php
@@ -1,3 +1,6 @@
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
 <ol class="breadcrumb">
   <li class="breadcrumb-item">
     <a href="<?php echo site_url('/admin') ?>">Dashboard</a>
@@ -21,11 +24,84 @@
   </div>
 </div>
 
-<input type="button" name="service-summary-btn" value="Edit Service Summary" id="service-summary-btn" class="btn-add" /> 
+
+<!-- <div class="container"> -->
+  <!-- Nav pills -->
+  <ul class="nav nav-pills" role="tablist">
+    <li class="nav-item">
+      <a class="nav-link btn-add active" data-toggle="pill" href="#home" name="service-summary-btn" value="Edit Service Summary" id="service-summary-btn" class="btn-add">SERVICE SUMMARY</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link btn-add" data-toggle="pill" href="#menu1" name="service-desc-btn" value="Edit Service Description" id="service-desc-btn" class="btn-add">SERVICE DESCRIPTION</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link btn-add" data-toggle="pill" href="#menu2">PHOTO DISPLAYED</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link btn-add" data-toggle="pill" href="#menu3">GALLERY</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link btn-add" data-toggle="pill" href="#menu4">AMENITIES</a>
+    </li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div id="home" class="tab-pane active"><br>
+      <div class="row">
+          <div class="col-md-12">
+            <fieldset class="fieldset-summary">
+              <legend for="services-summary" class="services-summary-legend">Service Summary</legend>
+              <textarea name="service-summary" class="form-control editor">
+                <?php echo $services['service_summary']; ?>
+              </textarea>
+            </fieldset>
+          </div>
+      </div>
+     <div class="service-button-row">
+       <button type="submit" class="btn btn-default btn-add">Submit</button>
+     </div>
+    </div>
+    <div id="menu1" class="tab-pane fade"><br>
+      <div class="row">
+          <div class="col-md-12">
+            <fieldset class="fieldset-desc">
+              <legend for="services-desc" class="services-desc-legend">Service Description</legend>
+              <textarea name="services-desc" class="form-control editor2">
+                <?php echo $services['services_desc']; ?>
+              </textarea>
+            </fieldset>
+          </div>
+      </div>
+     <div class="service-button-row">
+       <button type="submit" class="btn btn-default btn-add">Submit</button>
+     </div>
+    </div>
+    <div id="menu2" class="tab-pane fade"><br>
+      <div class="row">
+        <div class="col-md-3"></div>
+         <div class="col-sm-6">
+            <fieldset>
+              <legend for="services-img" class="services-img-legend">Image Displayed</legend>
+              <img src="<?php echo site_url().$services['img_src']; ?>" alt="" style="width: 100%; height: 100%;">
+              <?php echo form_open_multipart('admin/services/do_upload', "id='upload-image'");?>
+              <input type="hidden" name="id" value="<?php echo $services['ID']; ?>">
+              <input type="hidden" name="services-slug" value="<?php echo $services['services_slug']; ?>">
+              <input type="file" id="services-img" name="services-img">
+            </fieldset>
+          <?php echo form_close(); ?>
+        </div>
+        <div class="col-md-3"></div>
+      </div>  
+    </div>
+  </div>
+<!-- </div> -->
+
+<!-- <input type="button" name="service-summary-btn" value="Edit Service Summary" id="service-summary-btn" class="btn-add" /> 
 | 
 <input type="button" name="service-desc-btn" value="Edit Service Description" id="service-desc-btn" class="btn-add" />
-
-<div class="row">
+ -->
+<!-- <div class="row">
   <div class="col-sm-9">
 
     <fieldset class="fieldset-summary">
@@ -46,8 +122,8 @@
      <button type="submit" class="btn btn-default btn-add">Submit</button>
    </div>
    </form>
- </div>
- <div class="col-sm-3">
+ </div> -->
+<!--  <div class="col-sm-3">
     <fieldset>
       <legend for="services-img" class="services-img-legend">Image Displayed</legend>
       <img src="<?php echo site_url().$services['img_src']; ?>" alt="" style="width: 100%; height: 100%;">
@@ -58,13 +134,13 @@
     </fieldset>
   <?php echo form_close(); ?>
 
-</div>
+</div> -->
 </div>
 
 
 
 <script>
-	ClassicEditor
+  ClassicEditor
   .create( document.querySelector( '.editor' ), {
     toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
     heading: {
--- assets/admin/js/script.js
+++ assets/admin/js/script.js
@@ -4,15 +4,15 @@ $(function () {
     $('#services-img').parent().submit();
   });
 
-  $('#service-summary-btn').click(function(){
-    $('.fieldset-summary').css('display', 'block');
-    $('.fieldset-desc').css('display', 'none');
-  });
+  // $('#service-summary-btn').click(function(){
+  //   $('.fieldset-summary').css('display', 'block');
+  //   $('.fieldset-desc').css('display', 'none');
+  // });
 
-  $('#service-desc-btn').click(function(){
-    $('.fieldset-desc').css('display', 'block');
-    $('.fieldset-summary').css('display', 'none');
-  });
+  // $('#service-desc-btn').click(function(){
+  //   $('.fieldset-desc').css('display', 'block');
+  //   $('.fieldset-summary').css('display', 'none');
+  // });
 
 	$(window).on('load', function () { // makes sure the whole site is loaded 
 		$('#status').fadeOut(); // will first fade out the loading animation 
